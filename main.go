873e2045d32832638e52592b3adba4f41fde7415
Baris Bakla <baris.bakla@seven.one>
2024-06-16 20:56:43 +0200

    Swagger ui is working

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+
 	//openapigengin "github.com/bbakla/openapi3-with-go/openapi-generator/generated/oapi_generator_userapi"
 	openapigengin "github.com/bbakla/openoapi-code-generator/oapi_generator_userapi"
-	"github.com/gin-gonic/gin"
 	"net/http"
 
 	//openapigengin "github.com/bbakla/openapi3-with-go/openapi-generator/go-gen-gin/oapi_generator_userapi"
@@ -11,6 +13,9 @@ import (
 	"github.com/bbakla/openapi3-with-go/openapi-generator/openapigenonlyinterface"
 	"log"
 )
+
+//go:embed swagger-ui
+var content embed.FS
 
 func main() {
 
@@ -20,9 +25,12 @@ func main() {
 	log.Printf("Server started")
 	router := openapigengin.NewRouter(routes)
 
-	router.GET("/swagger", func(context *gin.Context) {
+	/*router.GET("/swagger", func(context *gin.Context) {
 		context.JSON(http.StatusOK, "hello swagger")
-	})
+
+	})*/
+	fsys, _ := fs.Sub(content, "swagger-ui")
+	router.StaticFS("/swagger", http.FS(fsys))
 
 	log.Fatal(router.Run(":8080"))
 
